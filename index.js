386b81c10b5eef6f704e875bbd3ea1a5d59273d4
AlbusSeverus <ericsonhnc@gmail.com>
2023-04-04 00:00:06 -0700

    init Repo

---
+++ index.js
@@ -0,0 +1,91 @@
+//  packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+// array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        message: 'What would you like your repository title to be?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What would you like your project name to be?',
+        name: 'name',
+    },
+    {
+        type: 'list',
+        message: 'Choose your license for your project.',
+        name: 'license',
+        choices: [
+            { value: 'Apache' },
+            { value: 'BSD3' },
+            { value: 'LGPL' },
+            { value: 'MIT' },
+            { value: 'MPL' },
+            { value: 'None' },
+        ]
+    },
+    {
+        type: 'input',
+        message: 'Provide a description of your application:',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the devlopment environment running.',
+        name: 'install',
+    },
+    {
+        type: 'input',
+        message: 'Provide instructions and examples for use. Include screenshots as needed.',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'How would you test this project?',
+        name: 'test',
+    },
+    {
+        type: 'input',
+        message: 'List your collaborates',
+        name: 'credits',
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username:',
+        name: 'github',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email:',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Where is this application deployed at?',
+        name: 'deploy',
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Success! Your README.md file has been generated!')
+    );
+}
+
+//function to initialize app
+function init() {
+    inquirer.prompt(questions)
+        .then((data) => {
+            writeToFile('README.md', generateMarkdown(data));
+        })
+        .catch((err) => {
+            console.log(err);
+        }
+        );
+}
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,78 @@
+// Function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function badge(license) {
+  if (license !== "None") {
+    return `![Badge](https://img.shields.io/badge/License-${license}-blue.svg)`
+  }
+  return ``;
+}
+
+// Function that returns license in table of contents
+ // If there is no license, return an empty string
+function link(license) {
+  if (license !== "None") {
+    return `- [License](#license-📄)`
+  }
+  return ``;
+}
+
+// Function that returns the license section of README
+// If there is no license, return an empty string
+
+function section(license) {
+  if (license !== "None") {
+    return `## License 📄
+    This application is covered by the ${license} license.`
+  }
+  return ``;
+
+}
+
+// Function to generate markdown for README
+
+
+function generateMarkdown(data) {
+return `
+# ${data.title}
+
+${badge(data.license)}
+
+${data.name}
+
+## Description 📃
+${data.description}
+
+## Table of Contents 📖
+- [Installation](#installation-🛠️)
+- [Usage](#usage-🕹️)
+${link(data.license)}
+- [Test](#tests-💉)
+- [Credits](#credits-©️)
+- [Questions](#questions-❓)
+- [Deployed Application](#deployed-application-🪂)
+
+## Installation 🛠️
+${data.install}
+
+## Usage 🕹️
+${data.usage}
+
+${section(data.license)}
+
+## Tests 💉
+${data.test}
+
+## Credits ©️
+${data.credits}
+
+## Questions ❓
+If there are any questions, feel free to contact my email at: ${data.email}
+
+You can also find me on GitHub at: [${data.github}](https://www.github.com/${data.github})
+
+## Deployed Application 🪂
+This application was deplopyed at [${data.deploy}](${data.deploy})
+`
+}
+
+module.exports = generateMarkdown;
